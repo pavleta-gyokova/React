701544f9a0e84172d4a5d6ce17202993f49f124a
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-14 19:02:15 +0300

    Update Activity.js

--- AirBnB Project - Part 1/src/utility/Activity/Activity.js
+++ AirBnB Project - Part 1/src/utility/Activity/Activity.js
@@ -3,13 +3,6 @@ import './Activity.css';
 import { Link } from 'react-router-dom';
 class Activity extends Component{
 
-    // activityType: "KAYAKING"
-    // cost: 399
-    // id: 1
-    // image: "https://airbnb-clone-prexel-images.s3.amazonaws.com/activities/kayakingTour.jpg"
-    // rating: 4.7
-    // title: "Kayaking adventure"
-    // totalRatings: 131
 
     render(){
         const { activityType, cost, id, image, rating, title, totalRatings } = this.props.activity;

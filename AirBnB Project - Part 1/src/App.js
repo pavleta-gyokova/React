98956b0e6be8d2e0199d50012db13194cc3b0c12
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-18 19:19:20 +0300

    Update App.js
    Added a SingleFullVenue route

--- AirBnB Project - Part 1/src/App.js
+++ AirBnB Project - Part 1/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './pages/Home/Home.js'
 import NavBar from './utility/NavBar/NavBar.js'
+import SingleFullVenue from './pages/SingleFullVenue/SingleFullVenue';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <Router>
         <Route path='/' component={NavBar}></Route>
         <Route exact path='/' component={Home}></Route>
+        <Route exact path='/venue/:vid' component={SingleFullVenue}></Route>
       </Router>
 
     );

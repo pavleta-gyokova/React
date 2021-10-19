17502572cc57d1ae62ce673d19e01c89f0b9471f
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-19 17:54:00 +0300

    Update App.js
    Adding a Modal

--- AirBnB Project - Part 1/src/App.js
+++ AirBnB Project - Part 1/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './pages/Home/Home.js'
 import NavBar from './utility/NavBar/NavBar.js'
 import SingleFullVenue from './pages/SingleFullVenue/SingleFullVenue';
+import Modal from './utility/Modal/Modal';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Route path='/' component={NavBar}></Route>
         <Route exact path='/' component={Home}></Route>
         <Route exact path='/venue/:vid' component={SingleFullVenue}></Route>
+        <Route path='/' component={Modal}></Route>
       </Router>
 
     );

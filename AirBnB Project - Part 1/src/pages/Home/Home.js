4b437393630756722d914ccef23c387b7eb5aab8
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-20 15:16:29 +0300

    Update Home.js

--- AirBnB Project - Part 1/src/pages/Home/Home.js
+++ AirBnB Project - Part 1/src/pages/Home/Home.js
@@ -88,7 +88,7 @@ class Home extends Component {
         }
 
         return (<>
-            <div className='container-fluid'>s
+            <div className='container-fluid'>
                 <div className='row'>
                     <div className='home col s12'>
                         <div className='upper-fold'>

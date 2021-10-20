006902575afff6cc9621357adb7b1d21154861f2
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-20 18:41:00 +0300

    Update authReducer.js
    Updated auth reducer

--- AirBnB Project - Part 1/src/reducers/authReducer.js
+++ AirBnB Project - Part 1/src/reducers/authReducer.js
@@ -1,3 +1,12 @@
-export default (state = [], action) =>{
-    return state;
+const initState = {}
+function authReducer(state = initState, action) {
+    if (action.type === 'REGISTER_ACTION') {
+        return action.payload;
+    } else if(action.type ==='LOGOUT'){
+        return initState;
+    } else {
+        return state;
+    }
 }
+
+export default authReducer;

f3159af8d132580a8f1d056d6cc2181c06eac1ba
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-20 18:42:32 +0300

    Update NavBar.js

--- AirBnB Project - Part 1/src/utility/NavBar/NavBar.js
+++ AirBnB Project - Part 1/src/utility/NavBar/NavBar.js
@@ -1,46 +1,68 @@
 import React, { Component } from 'react';
 import './navBar.css';
 import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import openModal from '../../actions/openModal';
 import Login from '../../pages/Login/Login';
 import SignUp from '../../pages/Login/SignUp';
+import logoutAction from '../../actions/logoutAction';
 
-class NavBar extends Component{
+class NavBar extends Component {
 
-    render(){
+    componentDidUpdate(oldProps){
+        if(oldProps.auth.token !== this.props.auth.token){
+            this.props.openModal('closed','')
+        }
+    }
+
+    render() {
 
         let navColor = 'transparent';
-        if(this.props.location.pathname !== '/'){
+        if (this.props.location.pathname !== '/') {
             navColor = 'black'
         }
-    
-        return(<div className="container-fluid nav">
-            <div className="row">
-                <nav className={navColor}>
-                    <div className="nav-wrapper">
-                        <Link to="/" className="left">airbnb</Link>
-                        <ul id="nav-mobile" className="right">
-                            <li><Link to="/">English (US)</Link></li>
-                            <li><Link to="/">$ USD</Link></li>
-                            <li><Link to="/">Become a host</Link></li>
-                            <li><Link to="/">Help</Link></li>
-                            <li onClick={()=>{this.props.openModal('open',<SignUp/>)}}>Sign up </li>
-                            <li onClick={()=>{this.props.openModal('open',<Login/>)}}> Log in</li>
-                        </ul>
-                    </div>
-                </nav>
+
+        return (
+            <div className="container-fluid nav">
+                <div className="row">
+                    <nav className={navColor}>
+                        <div className="nav-wrapper">
+                            <Link to="/" className="left"> Airbnb</Link>
+                            <ul id="nav-mobile" className="right">
+                                <li><Link to="/">English (US)</Link></li>
+                                <li><Link to="/">$ USD</Link></li>
+                                <li><Link to="/">Become a host</Link></li>
+                                <li><Link to="/">Help</Link></li>
+                                {this.props.auth.email
+                                    ? <>
+                                    <li className='login-signup'>Hello, {this.props.auth.email}</li>
+                                    <li className='login-signup' onClick={()=> this.props.logoutAction()}>Logout</li>
+                                    </>
+                                    : <>
+                                    <li className='login-signup' onClick={() => { this.props.openModal('open', <SignUp />) }}>Sign up </li>
+                                     <li className='login-signup' onClick={() => { this.props.openModal('open', <Login />) }}> Log in</li>
+                                    </>}
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
         )
     }
 }
 
-function mapDispatchToProps(dispatcher){
-    return bindActionCreators({
-        openModal: openModal
-    },dispatcher)
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    }
 }
 
-export default connect(null,mapDispatchToProps)(NavBar);
+function mapDispatchToProps(dispatcher) {
+    return bindActionCreators({
+        openModal: openModal,
+        logoutAction : logoutAction
+    }, dispatcher)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);

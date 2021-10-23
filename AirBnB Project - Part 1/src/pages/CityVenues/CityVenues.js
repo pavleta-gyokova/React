e86a5e80477b065bc93227b77bf43296a29c47af
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-10-23 19:28:13 +0300

    Add files via upload

---
+++ AirBnB Project - Part 1/src/pages/CityVenues/CityVenues.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './CityVenues.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+import Spinner from '../../utility/Spinner/Spinner';
+import Venues from '../../utility/Venue/Venues';
+
+class CityVenues extends Component {
+    state = {
+        header: '',
+        venues: []
+    }
+
+    async componentDidMount() {
+        const cityName = this.props.match.params.cityName;
+        const url = `${window.apiHost}/venues/city/${cityName}`;
+        const response = await axios.get(url, { cityName });
+        this.setState({
+            header: response.data.header,
+            venues: response.data.venues
+        })
+    }
+
+    render() {
+        if (!this.state.header) {
+            return <Spinner />
+        }
+
+        return (
+            <div className='row'>
+                <Venues venues={this.state.venues} header={this.state.header} />
+            </div>
+        )
+    }
+
+
+}
+
+export default CityVenues;

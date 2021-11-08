f938d5437f6b1904185e923a462d366130468aa2
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-11-08 16:13:23 +0200

    Delete Redux Middleware directory

--- Redux Middleware/src/reducers/weatherReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = {},action)=>{
-    if(action.type === "cityUpdate"){
-        console.log(action.payload)
-        return action.payload
-    }else{
-        return state;
-    }
-}

3a362e925fbdf90b2452100fc03ab2e1db32ce22
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-11-08 16:12:06 +0200

    Delete AirBnB Project - Part 1 directory

--- AirBnB Project - Part 1/src/actions/registerAction.js
+++
@@ -1,8 +0,0 @@
-function registerAction (regObj){
-    return{
-        type: "REGISTER_ACTION",
-        payload: regObj
-    }
-}
-
-export default registerAction;
--- AirBnB Project - Part 1/src/reducers/siteModal.js
+++
@@ -1,9 +0,0 @@
-const initState = {openClose : "closed", content : ''}
-
-function siteModule (state = initState, action){
-    if(action.type === 'OPEN_MODAL'){
-        return action.payload
-    }
-    return state;
-}
-export default siteModule;

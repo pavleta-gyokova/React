fd5b0e8fd0c13858fef4ef29f2ec6b529ef4ca6e
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2021-12-29 16:52:57 +0200

    Update CityWeatherHooks.js

--- WeatherApp Hooks/src/CityWeatherHooks.js
+++ WeatherApp Hooks/src/CityWeatherHooks.js
@@ -13,22 +13,23 @@ function CityWeatherHooks(props) {
     //takes 2 args:
     //1. a callback that runs...
     //2. 'when' to run 1
-        //1. undefined (no value) --> run EVERY render(componentDidMount + componentDidUpdate)
-        //2. [] --> run on the first render ONLY (componentDidMount)
-        //3. [...data] --> run on the first time and anytime a var in the array changes (componentDidMount +  modified componentDidUpdate)
-        //4.(sort of) - if you return a function, that will run when the component unmounts (componentWillUnmount)
+    //1. undefined (no value) --> run EVERY render(componentDidMount + componentDidUpdate)
+    //2. [] --> run on the first render ONLY (componentDidMount)
+    //3. [...data] --> run on the first time and anytime a var in the array changes (componentDidMount +  modified componentDidUpdate)
+    //4.(sort of) - if you return a function, that will run when the component unmounts (componentWillUnmount)
 
     useEffect(() => {
         const fetchWeather = async () => {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.cityName}&units=metric&appid=ab34560068dc014a04dae465e051823b`;
             const response = await axios.get(url);
             changeCityData(response.data);
+            console.log(response.data);
         };
         fetchWeather();
-    },[props.cityName]);
+    }, [props.cityName]);
 
-    if(!cityData.weather){
-        return(
+    if (!cityData.weather) {
+        return (
             <h1>Loading...</h1>
         )
     }
@@ -36,17 +37,17 @@ function CityWeatherHooks(props) {
     const iconUrl = `https://api.openweathermap.org/img/w/${cityData.weather[0].icon}.png`;
     return (
         <div className="container">
-                <div className='city-name'>
-                    <h4>{cityData.name} - {cityData.main.temp.toFixed(0)} &#x2103;</h4>
-                </div>
-                <div className='temp'>
-                    <img src={iconUrl} alt='icon' />
-                    {cityData.weather[0].description}
-                    <p>High: {cityData.main.temp_max.toFixed(0)} &#x2103; - Low: {cityData.main.temp_min.toFixed(0)} &#x2103;</p>
-                    <p>Feels like: {cityData.main.feels_like.toFixed(0)} &#x2103;</p>
-                </div>
-
+            <div className='city-name'>
+                <h4>{cityData.name} - {cityData.main.temp.toFixed(0)} &#x2103;</h4>
             </div>
+            <div className='temp'>
+                <img src={iconUrl} alt='icon' />
+                {cityData.weather[0].description}
+                <p>High: {cityData.main.temp_max.toFixed(0)} &#x2103; - Low: {cityData.main.temp_min.toFixed(0)} &#x2103;</p>
+                <p>Feels like: {cityData.main.feels_like.toFixed(0)} &#x2103;</p>
+            </div>
+
+        </div>
     )
 }
 export default CityWeatherHooks;

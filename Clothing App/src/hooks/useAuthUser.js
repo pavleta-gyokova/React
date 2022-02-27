9282a8065ba41ce3a195ff9120ea179c10c60140
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2022-02-27 19:50:02 +0200

    error fixing

--- Clothing App/src/hooks/useAuthUser.js
+++ Clothing App/src/hooks/useAuthUser.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import React, {useEffect} from 'react';
 import { auth, createUserProfileDocument } from '../firebase/firebase.utils';
 
 function useAuthUser(props) {

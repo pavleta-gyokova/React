11a69f58fa4e07fb051889d236c6194de9f2404c
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2022-03-01 23:16:01 +0200

    Redux hooks update

--- Clothing App/src/components/checkout-item/checkout-item.component.jsx
+++ Clothing App/src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import {
   clearItemFromCart,
@@ -9,7 +9,11 @@ import {
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+const CheckoutItem = ({ cartItem}) => {
+  const dispatch = useDispatch();
+  const clearItemClickHandler = (item) => dispatch(clearItemFromCart(item));
+  const addItemClickHandler = (item) => dispatch(addItem(item));
+  const removeItemClickHandler = (item) => dispatch(removeItem(item));
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
@@ -18,29 +22,20 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       </div>
       <span className='name'>{name}</span>
       <span className='quantity'>
-        <div className='arrow' onClick={() => removeItem(cartItem)}>
+        <div className='arrow' onClick={() => removeItemClickHandler(cartItem)}>
           &#10094;
         </div>
         <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => addItem(cartItem)}>
+        <div className='arrow' onClick={() => addItemClickHandler(cartItem)}>
           &#10095;
         </div>
       </span>
       <span className='price'>${price}</span>
-      <div className='remove-button' onClick={() => clearItem(cartItem)}>
+      <div className='remove-button' onClick={() => clearItemClickHandler(cartItem)}>
         &#10005;
       </div>
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item)),
-  addItem: item => dispatch(addItem(item)),
-  removeItem: item => dispatch(removeItem(item))
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(CheckoutItem);
+export default CheckoutItem;

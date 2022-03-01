7513679328600be83f783a1e4751e4da66784beb
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2022-03-01 23:16:52 +0200

    Redux hooks update

--- Clothing App/src/components/collection-item/collection-item.component.jsx
+++ Clothing App/src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({item}) => {
+  const dispatch = useDispatch();
+  const addItemClickHandler = (item) => dispatch(addItem(item));
   const { name, price, imageUrl } = item;
 
   return (
@@ -21,18 +23,11 @@ const CollectionItem = ({ item, addItem }) => {
         <span className='name'>{name}</span>
         <span className='price'>${price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton onClick={() => addItemClickHandler(item)} inverted>
         Add to cart
       </CustomButton>
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(item))
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(CollectionItem);
+export default CollectionItem;

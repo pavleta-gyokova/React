ca382116cb4eca2c3e640c2394cb01722d7984c6
Pavleta Guokowa <83221867+pavleta-gyokova@users.noreply.github.com>
2022-03-01 23:20:51 +0200

    Redux hooks update

--- Clothing App/src/pages/collection/collection.component.jsx
+++ Clothing App/src/pages/collection/collection.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
@@ -7,8 +8,9 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+const CollectionPage = () => {
+  const { collectionId } = useParams();
+  const { title, items } = useSelector(selectCollection(collectionId));
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
@@ -21,8 +23,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
